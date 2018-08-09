247f4af2953a60d08787f00783f21fb34f41a09f
Billy Wade <billy.wade@protonmail.ch>
2018-08-09 08:32:10 -0500

    Update main.cpp

--- es-app/src/main.cpp
+++ es-app/src/main.cpp
@@ -90,7 +90,7 @@ bool parseArgs(int argc, char* argv[])
 			Settings::getInstance()->setBool("ParseGamelistOnly", true);
 		}else if(strcmp(argv[i], "--ignore-gamelist") == 0)
 		{
-			Settings::getInstance()->setBool("IgnoreGamelist", false);
+			Settings::getInstance()->setBool("IgnoreGamelist", true);
 		}else if(strcmp(argv[i], "--show-hidden-files") == 0)
 		{
 			Settings::getInstance()->setBool("ShowHiddenFiles", true);
